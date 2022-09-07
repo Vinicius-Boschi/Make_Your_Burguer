04e91a9f10751794aecbf1f9b456e93a24333021
Vinicius Boschi <vinicius.yg@gmail.com>
2022-09-07 19:40:39 -0300

    Adicionando conteúdo ao projeto

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,46 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <Navbar :logo="logo_src" :alt="app_name" />
   <RouterView />
+  <Footer />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<script>
+  import Navbar from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  export default {
+    components: {
+      Navbar,
+      Footer
+    },
+    data() {
+      return {
+        logo_src: '/img/logo.png',
+        app_name: 'Make Your Burguer'
+      }
+    }
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+</script>
+
+<style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .main-container {
+    margin: 50px;
+    min-height: 335px;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
   }
-}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <Banner />
+    <div class="main-container">
+        <h1>Monte o seu burguer: </h1>
+        <BurgerForm />
+    </div>
 </template>
+
+<script>
+
+    import Banner from '../components/Banner.vue'
+    import BurgerForm from '../components/BurgerForm.vue'
+
+    export default {
+        name: "Home",
+        components: {
+            Banner,
+            BurgerForm
+        }
+    }
+</script>
