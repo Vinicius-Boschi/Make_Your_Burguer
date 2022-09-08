9a2e4ee2b790dd19e41f512f69d440b1debd4f7e
Vinicius Boschi <vinicius.yg@gmail.com>
2022-09-08 16:45:06 -0300

    Atualizando os selects da dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -22,9 +22,9 @@
                     </ul>
                 </div>
                 <div>
-                    <select name="status" id="status">
+                    <select name="status" id="status" @change="updateBurger($event, burger.id)">
                         <option value="">Selecione</option>
-                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                     </select>
                     <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
@@ -65,6 +65,20 @@
             const res = await req.json()
 
             this.getPedidos()
+        },
+        async updateBurger(event, id) {
+            const option = event.target.value
+            const dataJson = JSON.stringify({ status: option })
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: dataJson
+            })
+
+            const res = await req.json()
         }
       },
       mounted() {
